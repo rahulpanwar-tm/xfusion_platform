c6de23f905956bd5bf16e29d54182fe23c888678
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-02 20:06:56 +0530

    update all database

---
+++ xfusion_platform_2019-01-03.sql
@@ -0,0 +1,3 @@
+-- Dumping database structure for xfusion_performance_spider
+DROP DATABASE IF EXISTS `xfusion_auth_engine`;
+CREATE DATABASE IF NOT EXISTS `xfusion_auth_engine` /*!40100 DEFAULT CHARACTER SET utf8 */;
